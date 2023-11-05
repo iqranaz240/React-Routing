5a99dc179dda878e23f4953d02623b500c176168
iqranaz240 <iqranaz240@gmail.com>
2023-11-05 00:21:45 -0700

    Nested Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import NavBar from './components/Navbar';
+import PostLists from './components/PostList';
+import Posts from './pages/Posts';
+import Post from './pages/Post';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/posts" element={<Posts />}>
+            <Route path=":slug" element={<Post />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
